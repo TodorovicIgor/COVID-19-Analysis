9bba9d2e62caa05cde0c0b001a27c4d7c31e76f6
TodorovicIgor <40495825+TodorovicIgor@users.noreply.github.com>
2020-04-03 12:35:20 +0200

    added gompertz model

--- confirmed_cases_regression.py
+++ confirmed_cases_regression.py
@@ -5,6 +5,10 @@ from scipy.optimize import curve_fit
 from datetime import datetime, timedelta
 
 
+def gompertz_model(x, a, b, c):
+    return np.multiply(a, np.exp(-np.multiply(b, np.exp(-np.multiply(c, x)))))
+
+
 def logistic_model(x, a, b, c):
     return c/(1+np.exp(-(x-b)/a))
 
@@ -26,10 +30,13 @@ with open('time_series_covid19_confirmed_global.csv', 'r') as file:
         for index in range(4, len(row)):
             ydata[index-4] += int(row[index])
 
-popt, pcov = curve_fit(logistic_model, xdata, ydata, maxfev=5000)
-xdata_prediction = [i for i in range(int(2*popt[1]))]
+popt, pcov = curve_fit(gompertz_model, xdata, ydata, maxfev=5000)
+popt2, pcov2 = curve_fit(logistic_model, xdata, ydata, maxfev=5000)
+# xdata_prediction = [i for i in range(int(2*popt[1]))]  # logistic
+xdata_prediction = [i for i in range(250)]  # gompertz
 
-plt.plot(logistic_model(xdata_prediction, *popt), 'r--')
+plt.plot(gompertz_model(xdata_prediction, *popt), 'r--')
+plt.plot(logistic_model(xdata_prediction, *popt2), 'g--')
 plt.plot(ydata, 'b')
 plt.legend(['Forecast', 'Actual'], loc='upper left')
 plt.xlabel('Days since 22. January')
@@ -40,24 +47,24 @@ end = (begin + timedelta(days=int(2*popt[1]))).strftime('%m-%d-%Y')
 plt.title('Today\'s date: '+today+'\nInflection point: '+inflection+'\nEstimated end: '+end)
 
 # saving plot
-plt.savefig('output/regression/img/'+today+'.png', dpi=200)
-
-# appending data to .csv if not already appended
-with open('output/regression/regression_history.csv', 'r', newline='') as file:
-    reader = csv.reader(file)
-    new_day = True
-    for row in reader:
-        if row[0] == today:
-            new_day = False
-            break
-if new_day:
-    with open('output/regression/regression_history.csv', 'a', newline='') as file:
-        writer = csv.writer(file, delimiter=',')
-        row = [today, days_since_beginning, inflection, int(popt[1]), end, int(2*popt[1]), int(popt[2])]
-        # scientific notation messes everything up, solution is to traverse ydata array, apparently astype doesn't work
-        for item in ydata:
-            row.append(int(item))
-        writer.writerow(row)
+# plt.savefig('output/regression/img/'+today+'.png', dpi=200)
+#
+# # appending data to .csv if not already appended
+# with open('output/regression/regression_history.csv', 'r', newline='') as file:
+#     reader = csv.reader(file)
+#     new_day = True
+#     for row in reader:
+#         if row[0] == today:
+#             new_day = False
+#             break
+# if new_day:
+#     with open('output/regression/regression_history.csv', 'a', newline='') as file:
+#         writer = csv.writer(file, delimiter=',')
+#         row = [today, days_since_beginning, inflection, int(popt[1]), end, int(2*popt[1]), int(popt[2])]
+#         # scientific notation messes everything up, solution is to traverse ydata array, apparently astype doesn't work
+#         for item in ydata:
+#             row.append(int(item))
+#         writer.writerow(row)
 
 # plt.clf()
 plt.show()
